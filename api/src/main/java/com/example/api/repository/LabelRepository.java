4e634431019f8970d5dc67f188fac9bf0e3efb0d
Rareș-Ionuț Boza <bozarares@gmail.com>
2024-06-23 16:00:51 +0300

    Add repository files

---
+++ api/src/main/java/com/example/api/repository/LabelRepository.java
@@ -0,0 +1,9 @@
+package com.example.api.repository;
+
+import com.example.api.model.Label;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LabelRepository extends JpaRepository<Label, Long> {
+}
